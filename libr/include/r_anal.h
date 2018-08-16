6225410c9273a7923c868e38a81e464a26578cc9
Florian Märkl <info@florianmaerkl.de>
2018-08-16 18:36:33 +0200

    Adapt anal classes to vector changes

--- libr/include/r_anal.h
+++ libr/include/r_anal.h
@@ -686,7 +686,7 @@ typedef struct r_anal_t {
 #if R_ANAL_CLASSES_SDB
 	Sdb *sdb_classes;
 #else
-	RVector classes;
+	RPVector classes;
 #endif
 	//RList *hints; // XXX use better data structure here (slist?)
 	RAnalCallbacks cb;
@@ -1127,7 +1127,7 @@ typedef struct r_anal_class_t {
 	ut64 addr;
 	ut64 vtable_addr;
 	RVector base_classes; // <RAnalBaseClass>
-	RVector methods; // <RAnalMethod>
+	RPVector methods; // <RAnalMethod>
 } RAnalClass;
 
 typedef int (*RAnalEsilOp)(RAnalEsil *esil);
